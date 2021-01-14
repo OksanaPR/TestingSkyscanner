127491772fbe6029faf175ae217b63f5b7f34120
op671200@broadcom.net <oksana.prokopyak@broadcom.com>
2021-01-14 21:57:38 +0100

    Added delete random element keyword. Still IN PROGRESS...

--- Libraries/complexTestsLibrary.py
+++ Libraries/complexTestsLibrary.py
@@ -2,6 +2,7 @@ from robot.api.deco import keyword
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver import ActionChains
 import random
 import os
 
@@ -15,13 +16,33 @@ class complexTestsLibrary:
         os.environ['MOZ_HEADLESS'] = '1'
         self.driver = webdriver.Firefox()
         self.driver.get('http://todomvc.com/examples/react/#/')
+        self.driver.implicitly_wait(20)
+
+    @keyword
+    def close_browser(self):
+        self.driver.close()
 
     @keyword
     def generate_random_sentences(self, number, total_items_amount):
-        words = [["One", "Two", "Three", "Four"], ["red", "yellow", "green", "blue"], ["cats", "dogs", "zebras", "pandas"],["jumped.", "danced.", "wrote poetry.", "cried."]]
+        words = [["One", "Two", "Three", "Four"], ["red", "yellow", "green", "blue"],
+                 ["cats", "dogs", "zebras", "pandas"], ["jumped.", "danced.", "wrote poetry.", "cried."]]
         for i in range(int(number)):
             sentences = ' '.join([random.choice(w) for w in words])
             self.driver.find_element(By.CLASS_NAME, "new-todo").send_keys(sentences, Keys.ENTER)
+        total_items = self.driver.find_element(By.CLASS_NAME, "todo-count")
+        assert total_items.text == total_items_amount
+
+    @keyword
+    def delete_random_element(self, total_items_amount):
+        #could not find a way to select index randomly and use it when selecting list item I want to delete
+        #i = range(51) - because we added 50 items
+        #index = random.choice(i) - that supposed to select a namber from a range
+        list_item = self.driver.find_element(By.XPATH, '(//*[@class="view"])[2]')
+        hidden_button = self.driver.find_element(By.XPATH, '(//*[@class="destroy"])[2]')
+        actions = ActionChains(self.driver)
+        actions.move_to_element(list_item)
+        actions.click(hidden_button)
+        actions.perform()
         total_items = self.driver.find_element(By.CLASS_NAME, "todo-count")
         assert total_items.text == total_items_amount
 
--- Tests/complex_tests_with_library.robot
+++ Tests/complex_tests_with_library.robot
@@ -2,7 +2,10 @@
 Library  ../Libraries/complexTestsLibrary.py
 
 Test Setup  Open Browser
+Test Teardown  Close Browser
 
 *** Test Cases ***
 TC - Delete random item
   Generate Random Sentences  50  50 items left
+  Delete Random Element  49 items left
+
