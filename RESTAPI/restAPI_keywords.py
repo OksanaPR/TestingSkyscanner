bde5af6dce49c021ff34c409c00503f5aafa04da
OksanaPR <oksprokk@gmail.com>
2021-01-26 21:49:27 +0100

    Rest API tests, still in progress

--- RESTAPI/restAPI_keywords.py
+++ RESTAPI/restAPI_keywords.py
@@ -2,14 +2,43 @@ from robot.api.deco import keyword
 import requests
 import json
 import restAPI_library
+import jsonschema
 
 
 class restAPI_keywords:
     ROBOT_LIBRARY_SCOPE = 'TESTS'
     restAPI_library.get_method("https://swapi.dev/api/people/")
-    assert requests.status_codes == 200
+   # assert requests.status_codes == 200
     print(requests.status_codes)
 
+    @keyword
+    def validateDataResponse(self):
+        schemaToValidate = {
+            "type": "object",
+            "results": {
+                "name": {"type": "string"},
+                "height": {"type": "number"},
+                "mass": {"type": "number"}
+            }
+        }
+
+    def validateJson(jsonData):
+        try:
+            validate(instance=jsonData, schema=schemaToValidate)
+        except jsonschema.exceptions.ValidationError as err:
+            return False
+        return True
+
+        # Convert to Python object
+        jsonData = json.loads(requests.get("https://swapi.dev/api/people/"))
+        isValid = validateJson(jsonData)
+
+        if isValid:
+            print(jsonData)
+            print("Given JSON data is Valid")
+        else:
+            print(jsonData)
+            print("Given JSON data is InValid")
 
 
 
--- RESTAPI/restAPI_library.py
+++ RESTAPI/restAPI_library.py
@@ -5,6 +5,7 @@ def get_method(URL):
     try:
         response = requests.get(URL,timeout=3)
         response.raise_for_status()
+        assert response.status_code == 200
     except requests.exceptions.RequestException as error1:
         print("OOps: Something Else", error1)
     except requests.exceptions.HTTPError as error2:
@@ -13,7 +14,5 @@ def get_method(URL):
         print("Error Connecting:", error3)
     except requests.exceptions.Timeout as error4:
         print("Timeout Error:", error4)
-
-
 
 
